0e33b8d5d739126c1cf116353616015afb57fadd
brunats <you@example.com>
2016-05-24 14:42:08 -0300

    newB

--- mainB.h
+++ mainB.h
@@ -5,16 +5,6 @@
 #define MaxNameEnt 20
 
 typedef struct{
-    char nomeEntidade[MaxNameEnt];
-    int qtdCampos;
-    int tamHeader;
-    char **campo;#define MaxBufferConfig 50
-#define MaxBufferHeader 100
-#define MaxBufferHeaderInd 5000
-#define MaxBuffer 10000
-#define MaxNameEnt 40
-
-typedef struct{
     int qtdCampos,
     	tamHeader,
     	*tamanhos,
