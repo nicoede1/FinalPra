58bffcc5185722469b11c8bd2e82046416a2eb3b
ChrisRenka <chris.renka@gmail.com>
2016-05-30 15:57:20 -0300

    Add files via upload

--- mainB.h
+++ mainB.h
@@ -4,40 +4,39 @@
 #define MaxBuffer 10000
 #define MaxNameEnt 20
 
+/*
 typedef struct{
     int qtdCampos,
     	tamHeader,
     	*tamanhos,
-    	*null?,
-    	autoIncrement?;
+    	*null,
+    	autoIncrement;
     char 	**campo,
     		**tipo;
     char nomeEntidade[100];
 }Tabela;
+*/
 
-void inserir(int opcE, int opcO);
-void excluir(int opcE, int opcO);
-void atualizar(int opcE, int opcO);
-void buscar(int opcE, int opcO);
-int menuCrud(int opcE);
-
-
-void lerConfig(FILE *arqConfig, char *config);
-void createHeader();
-
-void createInd();
-int menuCrud(int opcE);
-    char **tipo;
-    int *tamanhos;
-    int **PK;
-    
+typedef struct{
+    int tamHeader;
+    //char nomeEntidade[100];
+    int qtdCampos;
+    char campos[6][30];
+    int tamanhos[6];
+    char tipos[6][4];
+    int null[6];
+    int autoInc[6];
+    int nPk;
+    char pk[3][10]; 
 }Tabela;
 
-void inserir(int opcE, int opcO);
-void excluir(int opcE, int opcO);
-void atualizar(int opcE, int opcO);
-void buscar(int opcE, int opcO);
+void inserir(int opcE);
+void excluir(int opcE);
+void atualizar(int opcE);
+void buscar(int opcE);
 int menuCrud(int opcE);
+
+Tabela lerHeader(FILE *arqEnt);
 
 
 void lerConfig(FILE *arqConfig, char *config);
@@ -45,3 +44,4 @@ void createHeader();
 
 void createInd();
 int menuCrud(int opcE);
+
