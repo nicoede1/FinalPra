e9a78e78e3ed2714cc0cbce2d6e2ee09e5e0c0c1
ChrisRenka <chris.renka@gmail.com>
2016-06-01 22:10:22 -0300

    Add files via upload

--- mainB.h
+++ mainB.h
@@ -3,6 +3,8 @@
 #define MaxBufferHeaderInd 5000
 #define MaxBuffer 10000
 #define MaxNameEnt 20
+#define MAX 4
+#define MIN 2
 
 /*
 typedef struct{
@@ -17,6 +19,8 @@ typedef struct{
 }Tabela;
 */
 
+int *valorAutoInc;
+
 typedef struct{
     int tamHeader;
     //char nomeEntidade[100];
@@ -30,6 +34,24 @@ typedef struct{
     char pk[3][10]; 
 }Tabela;
 
+struct btreeNode {
+      long long int val[MAX + 1], count;
+      struct btreeNode *link[MAX + 1];
+};
+
+struct btreeNode *root;
+
+/* creating new node */
+struct btreeNode * createNode(long long int val, struct btreeNode *child) {
+      struct btreeNode *newNode;
+      newNode = (struct btreeNode *)malloc(sizeof(struct btreeNode));
+      newNode->val[1] = val;
+      newNode->count = 1;
+      newNode->link[0] = root;
+      newNode->link[1] = child;
+      return newNode;
+}
+
 void inserir(int opcE);
 void excluir(int opcE);
 void atualizar(int opcE);
@@ -39,9 +61,34 @@ int menuCrud(int opcE);
 Tabela lerHeader(FILE *arqEnt);
 
 
-void lerConfig(FILE *arqConfig, char *config);
+//void lerConfig(FILE *arqConfig, char *config);
 void createHeader();
 
-void createInd();
-int menuCrud(int opcE);
+//void createInd();
+//int menuCrud(int opcE);
+
+//Árvore
+void addValToNode(long long int val, int pos, struct btreeNode *node, struct btreeNode *child);
+void splitNode (long long int val, long long int *pval, int pos, struct btreeNode *node, struct btreeNode *child, struct btreeNode **newNode);
+int setValueInNode(long long int val, long long int *pval, struct btreeNode *node, struct btreeNode **child);
+void insertion(long long int val);
+void copySuccessor(struct btreeNode *myNode, int pos);
+void removeVal(struct btreeNode *myNode, int pos);
+void doRightShift(struct btreeNode *myNode, int pos);
+void doLeftShift(struct btreeNode *myNode, int pos);
+void mergeNodes(struct btreeNode *myNode, int pos);
+void adjustNode(struct btreeNode *myNode, int pos);
+int delValFromNode(long long int val, struct btreeNode *myNode);
+void deletion(long long int val, struct btreeNode *myNode);
+void searching(long long int val, int *pos, struct btreeNode *myNode);
+void buscaIndice(long long int val, int *pos, struct btreeNode *myNode, long long int *v);
+void traversal(struct btreeNode *myNode);
+
+
+
+
+
+
+
+
 
